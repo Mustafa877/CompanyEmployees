b7d6e7cbcaa724882ed7e2ca36fe03356e40724f
Mustafa Haddad <79608345+Mustafa877@users.noreply.github.com>
2023-11-27 14:32:57 +0300

    - 13 VALIDATION     - ModelState, Rerun Validation, and Built-in Attributes     - Rerun Validation     - Built-in Attributes     - Custom Attributes and IValidatableObject     - Validation while Creating Resource     - Validating Int Type     - Validation for PUT Requests     - Validation for PATCH Requests

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -1,3 +1,4 @@
-﻿namespace Shared.DataTransferObjects;
+﻿
+namespace Shared.DataTransferObjects;
 
-public record EmployeeForCreationDto(string Name, int Age, string Position);
+public record EmployeeForCreationDto:EmployeeForManipulationDto;
--- Shared/DataTransferObjects/EmployeeForUpdateDto.cs
+++ Shared/DataTransferObjects/EmployeeForUpdateDto.cs
@@ -1,4 +1,5 @@
-﻿namespace Shared.DataTransferObjects;
+﻿
+namespace Shared.DataTransferObjects;
 
-public record EmployeeForUpdateDto(string Name, int Age, string Position);
+public record EmployeeForUpdateDto : EmployeeForManipulationDto;
 
